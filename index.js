681c64fb3ebf78e6d79e6ddeb83cf2d6be6a0908
Brendan Cordova <cordovalegacy19@gmail.com>
2023-10-01 17:19:00 -0700

    Done with sorting algo

--- index.js
+++ index.js
@@ -6,16 +6,21 @@ const array2 = [11, 19, 8, 7, 16, 5, 4]
 const sortArray = (arr) => {
     console.log("before: ", arr)
     let temp = 0
-    for(let i = 1; i < arr.length; i++){
-        if(arr[i] < arr[i-1]){
-            temp = arr[i-1]
-            arr[i-1] = arr[i]
-            arr[i] = temp
-            temp = 0
+    let swapping = true
+    while (swapping) {
+        swapping = false
+        for (let i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1]) {
+                temp = arr[i - 1]
+                arr[i - 1] = arr[i]
+                arr[i] = temp
+                temp = 0
+                swapping = true
+            }
         }
     }
     console.log("after: ", arr)
 }
 
-// sortArray(array)
-sortArray(array2)
+sortArray(array)
+// sortArray(array2)
